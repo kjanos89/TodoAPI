234281ac5da7a8fdf2edb54a3183758c67379baf
kjanos89 <91398032+kjanos89@users.noreply.github.com>
2023-08-25 22:46:21 +0200

    Removed an attribute that was left in after fixing a bug

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -29,7 +29,6 @@ namespace TodoAPI.Controllers
         }
 
         [HttpPost]
-        [HttpPost]
         public ActionResult<TodoItem> Create(TodoItem todoItem)
         {
             todoItem.Id = _nextId++;
